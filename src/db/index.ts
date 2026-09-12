9c769aa7d350a306de934873fbd59f93a1e7201d
saymdtechgroup <say.mdtechgroup@gmail.com>
2026-09-13 01:15:15 +0530

    Add files via upload

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,14 +8,22 @@ declare global {
 
 export const createPool = () => {
   if (!global._postgresPool) {
-    global._postgresPool = new Pool({
-      host: process.env.SQL_HOST,
-      user: process.env.SQL_USER,
-      password: process.env.SQL_PASSWORD,
-      database: process.env.SQL_DB_NAME,
-      max: 10,
-      connectionTimeoutMillis: 15000,
-    });
+    const connectionString = process.env.DATABASE_URL;
+
+    global._postgresPool = connectionString
+      ? new Pool({
+          connectionString,
+          max: 10,
+          connectionTimeoutMillis: 15000,
+        })
+      : new Pool({
+          host: process.env.SQL_HOST,
+          user: process.env.SQL_USER,
+          password: process.env.SQL_PASSWORD,
+          database: process.env.SQL_DB_NAME,
+          max: 10,
+          connectionTimeoutMillis: 15000,
+        });
 
     global._postgresPool.on('error', (err) => {
       console.error('Unexpected error on idle SQL pool client:', err);
